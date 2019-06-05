1d7940b35e1feac8b2871f0b84def68cd3fdfcef
1024小神 <648133599@qq.com>
2019-06-05 23:08:45 +0800

    通过工厂模式传参来确定开发环境

--- info/__init__.py
+++ info/__init__.py
@@ -5,14 +5,18 @@ from flask_wtf import CSRFProtect
 from redis import StrictRedis
 from infoconfig import config
 
-app = Flask(__name__)
-# 添加配置文件
-app.config.from_object(config["development"])
 # 初始化数据库
-db = SQLAlchemy(app)
-# 创建redis存储对象
-redis_store = StrictRedis(host=config["development"].REDIS_HOST, port=config["development"].REDIS_PORT)
-# 开启CSRF保护
-CSRFProtect(app)
-# 设置session保存的指定位置
-Session(app)
+db = SQLAlchemy()
+def create_app(config_name):
+    app = Flask(__name__)
+    # 添加配置文件
+    app.config.from_object(config[config_name])
+    # 创建redis存储对象
+    # 初始化数据库管理对象
+    db.init_app(app)
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    # 开启CSRF保护
+    CSRFProtect(app)
+    # 设置session保存的指定位置
+    Session(app)
+    return app
--- manger.py
+++ manger.py
@@ -1,8 +1,9 @@
 from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import app, db
+from info import create_app, db
 
+app = create_app("development")
 # 设置manger管理对象
 manager = Manager(app)
 # 配置数据库迁移设置,关联app和db

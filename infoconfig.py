8392e5bcb991739321408b8f4bebf26ca1a10a2c
1024小神 <648133599@qq.com>
2019-06-05 21:10:37 +0800

    抽取配置文件到单独文件中，因为manger只做入口操作

---
+++ infoconfig.py
@@ -0,0 +1,20 @@
+from redis import StrictRedis
+
+class Config(object):
+    SECRET_KEY = "asdadfafadfasdfa"
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql/127.0.0.1:3306/fnews"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # 配置redis的地址：
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    # 配置session存储在哪个数据库
+    SESSION_TYPE = "redis"
+    # 配置session的签名，让数据更安全
+    SESSION_USE_SIGNER = True
+    # 指定session保存的redis地址，
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置session需要过期否
+    SESSION_PERMANENT = False
+    # 这是过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
--- manger.py
+++ manger.py
@@ -1,29 +1,11 @@
-from flask import Flask,session
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
-class Config(object):
-    SECRET_KEY = "asdadfafadfasdfa"
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql/127.0.0.1:3306/fnews"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # 配置redis的地址：
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    # 配置session存储在哪个数据库
-    SESSION_TYPE = "redis"
-    # 配置session的签名，让数据更安全
-    SESSION_USE_SIGNER = True
-    # 指定session保存的redis地址，
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 设置session需要过期否
-    SESSION_PERMANENT = False
-    # 这是过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
+from infoconfig import Config
 
 app = Flask(__name__)
 # 添加配置文件
@@ -46,6 +28,7 @@ manager.add_command("db", MigrateCommand)
 
 @app.route("/")
 def index():
+    print(11111)
     session["age"] = "18"
     return "hello world"
 

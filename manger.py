c3e03eb4c4bc805134c307162494ef5541c2424a
1024小神 <648133599@qq.com>
2019-06-05 19:14:43 +0800

    添加了基本配置和初始化数据库

--- manger.py
+++ manger.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql/127.0.0.1:3306/fnews"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 app = Flask(__name__)
-
+# 添加配置文件
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():

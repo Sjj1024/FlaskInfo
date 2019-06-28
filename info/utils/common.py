6c08691d6be1b706d0382d5a977d1bda9acb95c3
1024小神 <15670339118@qq.com>
2019-06-28 14:55:12 +0800

    增加一个排行显示的过滤器

---
+++ info/utils/common.py
@@ -0,0 +1,10 @@
+def do_index_class(index):
+    """自定义过滤器，过滤点击排序html的class"""
+    if index == 0:
+        return "first"
+    elif index == 1:
+        return "second"
+    elif index == 2:
+        return "third"
+    else:
+        return ""

5fdbd34581655c4cc0ba2dd7bba1e842718e1b14
1024小神 <15670339118@qq.com>
2019-06-28 14:50:52 +0800

    实现右边栏点击排行显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, current_app, redirect, session
 
-from info.models import User
+from info.models import User, News
 from . import index_blu
-from info import redis_store
+from info import redis_store, constants
 
 
 @index_blu.route("/")
@@ -17,11 +17,26 @@ def index():
         except Exception as e:
             current_app.logger.error(e)
 
+    # 右侧的新闻排行的逻辑
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 定义一个空的字典列表，里面装的就是字典
+    news_dict_li = []
+    # 遍历对象列表，将对象的字典添加到字典列表中
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
+
     data = {
-        "user": user.to_dict() if user else None
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_li
     }
 
     return render_template('news/index.html', data=data)
+
 
 @index_blu.route("/favicon.ico")
 def favicon():

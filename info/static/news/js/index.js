13b4099851ad2204a8507d716e2c09970d78c57a
1024小神 <15670339118@qq.com>
2019-06-28 15:46:19 +0800

    滚动页面自动加载新闻数据

--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,10 +1,12 @@
-var currentCid = 0; // 当前分类 id
+var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
 var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
+    // 界面加载完成之后去加载新闻数据
+    updateNewsData()
     // 首页分类切换
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
@@ -26,24 +28,27 @@ $(function () {
 
     //页面滚动加载相关
     $(window).scroll(function () {
+
         // 浏览器窗口高度
         var showHeight = $(window).height();
+
         // 整个网页的高度
         var pageHeight = $(document).height();
+
         // 页面可以滚动的距离
         var canScrollHeight = pageHeight - showHeight;
+
         // 页面滚动了多少,这个是随着页面滚动实时变化的
         var nowScroll = $(document).scrollTop();
+
         if ((canScrollHeight - nowScroll) < 100) {
-            if (!house_data_querying) {
-                // 将`是否正在向后端查询新闻数据`的标志设置为真
-                house_data_querying = true;
-                // 如果当前页面数还没到达总页数
-                if(cur_page < total_page) {
-                    // 向后端发送请求，查询下一页新闻数据
-                    updateNewsData();
-                } else {
-                    house_data_querying = false;
+            // 判断页数，去更新新闻数据
+            if(!data_querying){
+                data_querying = true
+                // 如果当前页面数据小于总页数，才去加载数据
+                if (cur_page < total_page){
+                    cur_page += 1
+                    updateNewsData()
                 }
             }
         }
@@ -55,7 +60,7 @@ function updateNewsData() {
     var params = {
         "cid": currentCid,
         "page": cur_page
-    };
+    }
     $.get("/news_list", params, function (resp) {
         // 数据加载完毕，设置【正在加载数据】的变量为 false 代表当前没有在加载数据
         data_querying = false

77f2fb039c37dd00e21164fc7331ff775847448c
1024小神 <15670339118@qq.com>
2019-06-29 19:32:47 +0800

    完成个人资料修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,8 +1,37 @@
-from flask import render_template, g
+from flask import render_template, g, jsonify, request
 from werkzeug.utils import redirect
 
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 from . import profile_blu
+
+
+@profile_blu.route('/base_info', methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    # 不同的请求方式做不同的事情
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
+
+    # 代表是修改用户数据
+    # 1. 取到传入的参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 2. 校验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if gender not in ("WOMAN", "MAN"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    user = g.user
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
+
 
 @profile_blu.route('/info')
 @user_login_data
--- info/static/news/js/user_base_info.js
+++ info/static/news/js/user_base_info.js
@@ -4,13 +4,12 @@ function getCookie(name) {
 }
 
 $(function () {
-
     $(".base_info").submit(function (e) {
         e.preventDefault()
 
         var signature = $("#signature").val()
         var nick_name = $("#nick_name").val()
-        var gender = $(".gender").val()
+        var gender = $("input[type=radio]:checked").val()
 
         if (!nick_name) {
             alert('请输入昵称')
@@ -20,6 +19,30 @@ $(function () {
             alert('请选择性别')
         }
 
-        // TODO 修改用户信息接口
+        var params = {
+            "signature": signature,
+            "nick_name": nick_name,
+            "gender": gender
+        }
+
+        $.ajax({
+            url: "/user/base_info",
+            type: "post",
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 更新父窗口内容
+                    $('.user_center_name', parent.document).html(params['nick_name'])
+                    $('#nick_name', parent.document).html(params['nick_name'])
+                    $('.input_sub').blur()
+                }else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
     })
 })

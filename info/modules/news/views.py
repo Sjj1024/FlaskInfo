2eeaeb514c79cb934f233a878d253f096521f51d
1024小神 <15670339118@qq.com>
2019-06-29 16:03:56 +0800

    评论内容前后端实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,69 @@
 from flask import render_template, current_app, session, g, abort, jsonify, request
 
-from info import constants
-from info.models import User, News
+from info import constants, db
+from info.models import User, News, Comment
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
 from . import news_blu
+
+
+@news_blu.route('/news_comment', methods=["POST"])
+@user_login_data
+def comment_news():
+    """
+    评论新闻或者回复某条新闻下指定的评论
+    :return:
+    """
+
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1. 取到请求参数
+    news_id = request.json.get("news_id")
+    comment_content = request.json.get("comment")
+    parent_id = request.json.get("parent_id")
+
+    # 2. 判断参数
+    if not all([news_id, comment_content]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 查询新闻，并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
+
+    # 3. 初始化一个评论模型，并且赋值
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_content
+    if parent_id:
+        comment.parent_id = parent_id
+
+    # 添加到数据库
+    # 为什么要自己去commit()?，因为在return的时候需要用到 comment 的 id
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+
+    return jsonify(errno=RET.OK, errmsg="OK", data=comment.to_dict())
 
 
 @news_blu.route("/news_collect", methods=['POST'])
--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -74,12 +74,69 @@ $(function(){
 
     })
 
-        // 评论提交
-    $(".comment_form").submit(function (e) {
+    // 评论提交
+     $(".comment_form").submit(function (e) {
         e.preventDefault();
+        var news_id = $(this).attr('data-newsid')
+        var news_comment = $(".comment_input").val();
+
+        if (!news_comment) {
+            alert('请输入评论内容');
+            return
+        }
+        var params = {
+            "news_id": news_id,
+            "comment": news_comment
+        };
+        $.ajax({
+            url: "/news/news_comment",
+            type: "post",
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == '0') {
+                    var comment = resp.data
+                    // 拼接内容
+                    var comment_html = ''
+                    comment_html += '<div class="comment_list">'
+                    comment_html += '<div class="person_pic fl">'
+                    if (comment.user.avatar_url) {
+                        comment_html += '<img src="' + comment.user.avatar_url + '" alt="用户图标">'
+                    }else {
+                        comment_html += '<img src="../../static/news/images/person01.png" alt="用户图标">'
+                    }
+                    comment_html += '</div>'
+                    comment_html += '<div class="user_name fl">' + comment.user.nick_name + '</div>'
+                    comment_html += '<div class="comment_text fl">'
+                    comment_html += comment.content
+                    comment_html += '</div>'
+                    comment_html += '<div class="comment_time fl">' + comment.create_time + '</div>'
+
+                    comment_html += '<a href="javascript:;" class="comment_up fr" data-commentid="' + comment.id + '" data-newsid="' + comment.news_id + '">赞</a>'
+                    comment_html += '<a href="javascript:;" class="comment_reply fr">回复</a>'
+                    comment_html += '<form class="reply_form fl" data-commentid="' + comment.id + '" data-newsid="' + news_id + '">'
+                    comment_html += '<textarea class="reply_input"></textarea>'
+                    comment_html += '<input type="button" value="回复" class="reply_sub fr">'
+                    comment_html += '<input type="reset" name="" value="取消" class="reply_cancel fr">'
+                    comment_html += '</form>'
+
+                    comment_html += '</div>'
+                    // 拼接到内容的前面
+                    $(".comment_list_con").prepend(comment_html)
+                    // 让comment_sub 失去焦点
+                    $('.comment_sub').blur();
+                    // 清空输入框内容
+                    $(".comment_input").val("")
+                }else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
 
     })
-
     $('.comment_list_con').delegate('a,input','click',function(){
 
         var sHandler = $(this).prop('class');
